2c48bcbde2740aad16a39fab7a369204c736ef2e
Boffin-ux <fnamily@mail.ru>
2021-05-28 20:08:27 +0300

    Выполнил обязательное задание Урок №2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+const money = 45000;
+const income = 'фриланс';
+const addExpenses = 'Интернет, Такси, Коммуналка';
+const deposit = true;
+const mission = 500000;
+const period = 6;
 
-alert('Hello World');
-console.log('Test');
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+const arrExpenses = addExpenses.toLowerCase().split(', ');
+console.log('addExpenses: ', arrExpenses);
+
+const budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
