7906293ec408c20c4c71442d4ee5e6cebc60787a
Boffin-ux <fnamily@mail.ru>
2021-05-31 22:11:00 +0300

    Исправил ошибки в основном задании Урок №3

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ console.log('addExpenses: ', arrExpenses);
 let budgetDay = money / 30;
 console.log('budgetDay: ', budgetDay);
 
-money = prompt('Ваш месячный доход?');
+money = +prompt('Ваш месячный доход?');
 addExpenses = prompt(
    'Перечислите возможные расходы за рассчитываемый период через запятую');
 deposit = confirm('Есть ли у вас депозит в банке?');
@@ -27,22 +27,22 @@ deposit = confirm('Есть ли у вас депозит в банке?');
 const expensesOne = prompt('1. Введите обязательную статью расходов?');
 const expensesTwo = prompt('2. Введите обязательную статью расходов?');
 
-const amountOne = prompt('1. Во сколько это обойдется?');
-const amountTwo = prompt('2. Во сколько это обойдется?');
+const amountOne = +prompt('1. Во сколько это обойдется?');
+const amountTwo = +prompt('2. Во сколько это обойдется?');
 
-const budgetMonth = money - expensesOne - expensesTwo;
+const budgetMonth = money - (amountOne + amountTwo);
 console.log('Бюджет на месяц: ', budgetMonth);
 
 console.log('Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
 
-budgetDay = budgetMonth / 30;
-console.log('Бюджет на день: ', Math.floor(budgetDay));
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('Бюджет на день: ', budgetDay);
 
 if (budgetDay >= 1200) {
    console.log('У вас высокий уровень дохода');
-} else if (budgetDay > 600 && budgetDay < 1200) {
+} else if (budgetDay >= 600 && budgetDay < 1200) {
    console.log('У вас средний уровень дохода');
-} else if (budgetDay > 0 && budgetDay < 600) {
+} else if (budgetDay >= 0 && budgetDay < 600) {
    console.log('К сожалению у вас уровень дохода ниже среднего');
 } else {
    console.log('Что то пошло не так');
