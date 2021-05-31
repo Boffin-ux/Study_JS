62bb6de1fcc9bbf288da76b62edc90a1d7c7e2e0
Boffin-ux <fnamily@mail.ru>
2021-05-31 10:33:14 +0300

    Выполнил п. 9-11 обязательного задания Урок №3

--- js/script.js
+++ js/script.js
@@ -31,7 +31,19 @@ const amountOne = prompt('1. Во сколько это обойдется?');
 const amountTwo = prompt('2. Во сколько это обойдется?');
 
 const budgetMonth = money - expensesOne - expensesTwo;
+console.log('Бюджет на месяц: ', budgetMonth);
+
 console.log('Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
 
 budgetDay = budgetMonth / 30;
 console.log('Бюджет на день: ', Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+   console.log('У вас высокий уровень дохода');
+} else if (budgetDay > 600 && budgetDay < 1200) {
+   console.log('У вас средний уровень дохода');
+} else if (budgetDay > 0 && budgetDay < 600) {
+   console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+   console.log('Что то пошло не так');
+}
